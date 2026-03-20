55e2ae46b94b8a67fe94ed6e0bf76f353d8c24b2
amarchino <marchino.alessandro@gmail.com>
2026-03-20 22:17:14 +0000

    Lesson 33

--- first-app/investment_calculator.go
+++ first-app/investment_calculator.go
@@ -11,18 +11,21 @@ func main() {
 	var years float64
 	var expectedReturnRate float64
 
-	fmt.Print("Investment amount: ")
+	outputText("Investment amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Expected return rate: ")
+	outputText("Expected return rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Inverment duration in years: ")
+	outputText("Inverment duration in years: ")
 	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1.0 + expectedReturnRate / 100, years)
 	futureRealValue := futureValue / math.Pow(1 + INFLATION / 100, years)
 
-	fmt.Printf(`Future value: %.2f
-	Future value (adjusted for inflation): %.2f`, futureValue, futureRealValue)
+	fmt.Printf("Future value: %.2f\nFuture value (adjusted for inflation): %.2f\n", futureValue, futureRealValue)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
 }
