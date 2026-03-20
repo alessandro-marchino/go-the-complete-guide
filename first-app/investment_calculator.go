65a47b2dda4a42373878d83eb055637788578bc6
amarchino <marchino.alessandro@gmail.com>
2026-03-20 17:34:53 +0000

    Lesson 25

--- first-app/investment_calculator.go
+++ first-app/investment_calculator.go
@@ -8,10 +8,17 @@ import (
 func main() {
 	const INFLATION = 2.5
 	var investmentAmount float64
-	years := 10.0
-	expectedReturnRate := 5.5
+	var years float64
+	var expectedReturnRate float64
 
+	fmt.Print("Investment amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Inverment duration in years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1.0 + expectedReturnRate / 100, years)
 	futureRealValue := futureValue / math.Pow(1 + INFLATION / 100, years)
