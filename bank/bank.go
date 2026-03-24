f12bc1dd9f07c4c6d38460f23083d627c12c90ca
amarchino <marchino.alessandro@gmail.com>
2026-03-24 22:20:53 +0000

    Lesson 58

--- bank/bank.go
+++ bank/bank.go
@@ -1,34 +1,13 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"os"
-	"strconv"
 )
 
 const ACCOUNT_BALANCE_FILE = "balance.txt"
 
-func writeFloatToFile(value float64, fileName string) {
-	balanceText := fmt.Sprint(value)
-	os.WriteFile(fileName, []byte(balanceText), 0644)
-}
-
-func getFloatFromFile(fileName string) (float64, error) {
-	data, err := os.ReadFile(fileName)
-	if err != nil {
-		return 1000, errors.New("Failed to find file")
-	}
-	valueText := string(data)
-	value, err := strconv.ParseFloat(valueText, 64)
-	if err != nil {
-		return 1000, errors.New("Failed to parse stored value")
-	}
-	return value, nil
-}
-
 func main() {
-	var accountBalance, err = getFloatFromFile(ACCOUNT_BALANCE_FILE)
+	var accountBalance, err = fileops.getFloatFromFile(ACCOUNT_BALANCE_FILE)
 	if err != nil {
 		fmt.Println("ERROR!")
 		fmt.Println(err)
@@ -58,7 +37,7 @@ func main() {
 			}
 			accountBalance += depositAmount
 			fmt.Printf("Balance updated|\nour balance is %.2f\n", accountBalance)
-			writeFloatToFile(accountBalance, ACCOUNT_BALANCE_FILE)
+			fileops.writeFloatToFile(accountBalance, ACCOUNT_BALANCE_FILE)
 		case 3:
 			fmt.Print("Withdraw amount: ")
 			var withdrawnAmount float64
@@ -73,7 +52,7 @@ func main() {
 			}
 			accountBalance -= withdrawnAmount
 			fmt.Printf("Balance updated|\nour balance is %.2f\n", accountBalance)
-			writeFloatToFile(accountBalance, ACCOUNT_BALANCE_FILE)
+			fileops.writeFloatToFile(accountBalance, ACCOUNT_BALANCE_FILE)
 		default:
 			fmt.Println("Goodbye!")
 			fmt.Println("Thanks for choosing our bank")
---
+++ bank/fileops/fileops.go
@@ -0,0 +1,26 @@
+package fileops
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func writeFloatToFile(value float64, fileName string) {
+	balanceText := fmt.Sprint(value)
+	os.WriteFile(fileName, []byte(balanceText), 0644)
+}
+
+func getFloatFromFile(fileName string) (float64, error) {
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return 1000, errors.New("Failed to find file")
+	}
+	valueText := string(data)
+	value, err := strconv.ParseFloat(valueText, 64)
+	if err != nil {
+		return 1000, errors.New("Failed to parse stored value")
+	}
+	return value, nil
+}
