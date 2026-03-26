18fcf3a0b26145edf2110570c567749a4d4e92bb
amarchino <marchino.alessandro@gmail.com>
2026-03-26 21:54:08 +0000

    Lesson 67

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	age := 32
+	agePointer := &age
 
 	fmt.Println("Age:", age)
+	fmt.Println("AgePointer:", agePointer)
+	fmt.Println("Age:", *agePointer)
 	fmt.Println(getAdultYears(age))
 }
 
