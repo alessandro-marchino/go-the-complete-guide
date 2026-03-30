4df8b00f5bf42e9ee92b81ec95b2229e792c7ff7
amarchino <marchino.alessandro@gmail.com>
2026-03-30 21:07:58 +0000

    Lesson 93

--- practice/structs/main.go
+++ practice/structs/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"example.com/practice/structs/note"
 )
@@ -17,13 +20,21 @@ func main() {
 }
 
 func getNodeData() (title, content string) {
-	title = getUserInput("Note title: ")
-	content = getUserInput("Note content: ")
+	title = getUserInput("Note title:")
+	content = getUserInput("Note content:")
 	return title, content
 }
 
 func getUserInput(prompt string) (value string) {
-	fmt.Print(prompt)
-	fmt.Scanln(&value)
+	fmt.Printf("%v ", prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+	text = strings.TrimSuffix(text, "\n")
+	value = strings.TrimSuffix(text, "\r")
 	return value
 }
