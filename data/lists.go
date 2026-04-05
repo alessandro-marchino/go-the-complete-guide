20b220ea4c8de859800815697ce470c5cd3c0eb7
amarchino <marchino.alessandro@gmail.com>
2026-04-05 21:05:21 +0000

    Lesson 111

--- data/lists.go
+++ data/lists.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
 	prices := [4]float64 {10.99, 9.99, 45.99, 20.0}
 	fmt.Println(prices)
+	fmt.Println(prices[2])
+
+	var productNames [4]string = [4]string{"A Book"}
+	productNames[2] = "A Carpet"
+	fmt.Println(productNames)
 }
