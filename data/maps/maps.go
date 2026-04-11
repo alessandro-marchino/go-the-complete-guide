cd73c85ae8d2986670fb2f729d13ef71dfc668c7
amarchino <marchino.alessandro@gmail.com>
2026-04-11 22:01:19 +0000

    Lesson 124

--- data/maps/maps.go
+++ data/maps/maps.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func main() {
 	websites := map[string]string{
 		"Google": "https://google.com",
@@ -16,9 +22,9 @@ func main() {
 	delete(websites, "Google")
 	fmt.Println(websites)
 
-	courseRatings := make(map[string]float64, 3)
+	courseRatings := make(floatMap, 3)
 	courseRatings["go"] = 4.7
 	courseRatings["react"] = 4.8
 	courseRatings["angular"] = 4.7
-	fmt.Println(courseRatings)
+	courseRatings.output()
 }
