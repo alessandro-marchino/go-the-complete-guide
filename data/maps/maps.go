e51e2f3cf54ab24ead792e5ab4740d00cb45a09f
amarchino <marchino.alessandro@gmail.com>
2026-04-11 21:58:29 +0000

    Lesson 123

--- data/maps/maps.go
+++ data/maps/maps.go
@@ -15,4 +15,10 @@ func main() {
 
 	delete(websites, "Google")
 	fmt.Println(websites)
+
+	courseRatings := make(map[string]float64, 3)
+	courseRatings["go"] = 4.7
+	courseRatings["react"] = 4.8
+	courseRatings["angular"] = 4.7
+	fmt.Println(courseRatings)
 }
