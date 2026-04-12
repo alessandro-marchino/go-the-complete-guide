20cae0235e808a61cded01164df1219473259b7f
amarchino <marchino.alessandro@gmail.com>
2026-04-12 19:25:35 +0000

    Lesson 127

---
+++ functions/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type transformFn func(int) int
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	doubled := transformNumbers(&numbers, double)
+	fmt.Println(doubled)
+
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println(tripled)
+}
+
+func transformNumbers(numbers *[]int, transform transformFn) (dNumbers []int) {
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
+	}
+	return dNumbers
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
