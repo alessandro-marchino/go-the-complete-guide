6dbb74a2e30bf5faa1d8b9d511616a2342ffeab6
amarchino <marchino.alessandro@gmail.com>
2026-04-17 20:38:06 +0000

    Lesson 143

--- practice/price_calculator/filemanager/filemanager.go
+++ practice/price_calculator/filemanager/filemanager.go
@@ -2,6 +2,7 @@ package filemanager
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"os"
 )
@@ -25,4 +26,19 @@ func ReadLines(path string) ([]string, error) {
 		return nil, errors.New("Failed to read line in file")
 	}
 	return lines, nil
+}
+
+func WriteJSON(path string, data any) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return errors.New("Failed to create file")
+	}
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(data)
+	if err != nil {
+		file.Close()
+		return errors.New("Failed to convert data to JSON")
+	}
+	file.Close()
+	return nil
 }
