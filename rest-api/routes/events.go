90facda2b46b6661feee75f47f98c04d111e74af
amarchino <marchino.alessandro@gmail.com>
2026-04-19 17:22:34 +0000

    Lesson 177

--- rest-api/routes/events.go
+++ rest-api/routes/events.go
@@ -75,3 +75,22 @@ func updateEvent(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusOK, gin.H{ "message": "Event updated!", "event": updatedEvent })
 }
+
+func deleteEvent(ctx *gin.Context) {
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{ "message": "Could not parse event id" })
+		return
+	}
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{ "message": "Could not fetch event" })
+		return
+	}
+	err = event.Delete()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{ "message": "Could not delete event" })
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{ "message": "Event deleted!" })
+}
