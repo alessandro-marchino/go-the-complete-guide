b796a7317ae51790e4320e70a96dfacbd0325e43
amarchino <marchino.alessandro@gmail.com>
2026-04-19 15:50:48 +0000

    Lesson 170

--- rest-api/db/db.go
+++ rest-api/db/db.go
@@ -17,4 +17,23 @@ func InitDB() {
 
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
+
+	createTables()
+}
+
+func createTables() {
+	createEventsTable := `
+		CREATE TABLE IF NOT EXISTS events (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			description TEXT NOT NULL,
+			location TEXT NOT NULL,
+			date_time DATETIME NOT NULL,
+			user_id INTEGER
+		)
+	`
+	_, err := DB.Exec(createEventsTable)
+	if err != nil {
+		panic("Could not create events table")
+	}
 }
