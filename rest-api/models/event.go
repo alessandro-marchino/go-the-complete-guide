f3a5740b2e1498a2b289d99d5855151e952cc15e
amarchino <marchino.alessandro@gmail.com>
2026-04-19 15:56:31 +0000

    Lesson 170

--- rest-api/models/event.go
+++ rest-api/models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/rest_api/db"
+)
 
 type Event struct {
-	ID int `json:"id"`
+	ID int64 `json:"id"`
 	Name string `json:"name" binding:"required"`
 	Description string `json:"description" binding:"required"`
 	Location string `json:"location" binding:"required"`
@@ -13,9 +17,25 @@ type Event struct {
 
 var events = []Event {}
 
-func (e *Event) Save() {
-	// TODO: Add to a database
-	events = append(events, *e)
+func (e *Event) Save() error {
+	query := `
+	INSERT INTO events (name, description, location, date_time, user_id)
+	VALUES (?, ?, ?, ?, ?)
+	`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	res, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+	e.ID = id
 }
 
 func GetAllEvents() []Event {
