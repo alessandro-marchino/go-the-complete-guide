031b7ff104db9d1298d028d651cffc7bc3935b0c
amarchino <marchino.alessandro@gmail.com>
2026-04-20 19:52:11 +0000

    Lesson 196

--- rest-api/routes/register.go
+++ rest-api/routes/register.go
@@ -29,5 +29,18 @@ func registerForEvent(ctx *gin.Context) {
 }
 
 func cancelRegistration(ctx *gin.Context) {
-
+	userId := ctx.GetInt64("userId")
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{ "message": "Could not parse event id" })
+		return
+	}
+	var event models.Event
+	event.ID = eventId
+	err = event.CancelRegistration(userId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{ "message": "Could not delete registration to the event" })
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{ "message": "Registration deleted" })
 }
